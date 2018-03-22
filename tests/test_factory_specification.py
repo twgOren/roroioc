fce85e129dff75d4c7388f8232e7d82dea66744c
orenovadia <oren@twiggle.com>
2018-03-22 16:47:05 +0200

    Test for attr type factory specs

--- tests/test_factory_specification.py
+++ tests/test_factory_specification.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from attr import attrs, attrib, Factory
+
 from roro_ioc.factory_inspection import extract_factory_specification, FactorySpecification
 
 
@@ -27,11 +29,22 @@ class TestFactorySpecification(TestCase):
         self.assertEqual({'y': 1, 'z': 'foo'}, specification.argument_default_values)
 
     def test_specification_for_init_if_type(self):
+        @attrs
+        class _MockAttrClass(object):
+            no_default = attrib()
+            string_default = attrib(default='default_string')
+            factory_default = attrib(default=Factory(dict))
+
+        specification = extract_factory_specification(_MockAttrClass)
+        self.assertItemsEqual(('no_default', 'string_default', 'factory_default'), specification.argument_names)
+        self.assertIn('factory_default', specification.argument_default_values)
+        self.assertEqual('default_string', specification.argument_default_values['string_default'])
+
+    def test_specification_for_attrs_object(self):
         class _MockClass(object):
             pass
 
         specification = extract_factory_specification(_MockClass)
-        self.assertIsNot(_MockClass, specification.constructing_function)
         self.assertIs(_MockClass.__init__, specification.constructing_function)
 
     def test_degenerate_specification_for_non_callable(self):
