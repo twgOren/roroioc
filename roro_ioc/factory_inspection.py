90036f5b804038e33c856ed00205ac028b03670b
orenovadia <oren@twiggle.com>
2018-03-22 16:33:00 +0200

    Adding tests for factory specification

--- roro_ioc/factory_inspection.py
+++ roro_ioc/factory_inspection.py
@@ -3,9 +3,8 @@ from inspect import getargspec
 from logging import getLogger
 
 import attr
-from typing import Callable
-
-frozendict = dict  # todo: change to immutable dict
+from typing import Callable, Dict, Any, Tuple
+from werkzeug.datastructures import ImmutableDict
 
 _logger = getLogger(__name__)
 
@@ -15,15 +14,15 @@ class FactorySpecification(object):
     constructing_function = attr.attrib()  # type: Callable
     argument_names = attr.attrib(validator=attr.validators.instance_of(tuple))  # type: Tuple[basestring, ...]
     argument_default_values = attr.attrib(validator=attr.validators.instance_of(
-        frozendict))  # type: Dict[basestring, Any]
+        ImmutableDict))  # type: Dict[basestring, Any]
 
 
 def _format_defaults(arg_names, defaults):
     defaults = defaults or ()
     defaults_matching = (attr.NOTHING,) * (len(arg_names) - len(defaults)) + defaults
-    return frozendict((arg_name, default_value) for (arg_name, default_value)
-                      in zip(arg_names, defaults_matching)
-                      if default_value != attr.NOTHING)
+    return ImmutableDict((arg_name, default_value) for (arg_name, default_value)
+                         in zip(arg_names, defaults_matching)
+                         if default_value != attr.NOTHING)
 
 
 def extract_factory_specification_for_attrs(type, allow_defaults):
@@ -32,15 +31,15 @@ def extract_factory_specification_for_attrs(type, allow_defaults):
     (fields_for_names, fields_for_defaults) = itertools.tee(fields, 2)
 
     argument_names = tuple(field.name for field in fields_for_names)
-    defaults = frozendict((field.name, field.default) for field in fields_for_defaults
-                          if field.default != attr.NOTHING)
+    defaults = ImmutableDict((field.name, field.default) for field in fields_for_defaults
+                             if field.default != attr.NOTHING)
     subject_callable = type
     return FactorySpecification(subject_callable, argument_names, defaults)
 
 
 def _extract_factory_specification_for_functions(functional_object):
     argument_names = ()
-    defaults = frozendict()
+    defaults = ImmutableDict()
     try:
         argspec = getargspec(functional_object)
         argument_names = tuple(argspec.args)
@@ -52,6 +51,7 @@ def _extract_factory_specification_for_functions(functional_object):
 
 
 def extract_factory_specification(type_or_factory, allow_defaults=True):
+    # type: (Callable, bool) -> FactorySpecification
     if isinstance(type_or_factory, type):
         try:
             return extract_factory_specification_for_attrs(type_or_factory, allow_defaults)
---
+++ tests/test_factory_specification.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from roro_ioc.factory_inspection import extract_factory_specification, FactorySpecification
+
+
+def _mock_func(x):
+    return x
+
+
+def _mock_func_with_kwargs(x, y=1, z='foo'):
+    return x
+
+
+class TestFactorySpecification(TestCase):
+    def test_degenerate(self):
+        specification = extract_factory_specification(_mock_func)
+        self.assertIsInstance(specification, FactorySpecification)
+        self.assertIs(_mock_func, specification.constructing_function)
+
+    def test_argument_names(self):
+        specification = extract_factory_specification(_mock_func)
+        self.assertItemsEqual(['x'], specification.argument_names)
+
+    def test_kwargs(self):
+        specification = extract_factory_specification(_mock_func_with_kwargs)
+        self.assertItemsEqual(['x', 'y', 'z'], specification.argument_names)
+        self.assertEqual({'y': 1, 'z': 'foo'}, specification.argument_default_values)
+
+    def test_specification_for_init_if_type(self):
+        class _MockClass(object):
+            pass
+
+        specification = extract_factory_specification(_MockClass)
+        self.assertIsNot(_MockClass, specification.constructing_function)
+        self.assertIs(_MockClass.__init__, specification.constructing_function)
