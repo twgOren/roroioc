49b2bd986b79d6ac2c2d102041d694f7b70c3e9d
orenovadia <orenovad@gmail.com>
2018-03-28 17:11:26 +0300

    Added long description for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup
 
+
+def readfile(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+readme = readfile('README.md')
+license = readfile('LICENSE')
+
 setup(
     name='roro_ioc',
     packages=['roro_ioc'],
-    version='0.1.3',
+    version='0.1.4',
     description='IOC Injection for python',
+    long_description='{}\n\n{}'.format(readme, license),
     author='Twiggle',
     author_email='oren@twiggle.com',
     url='https://github.com/twgOren/roroioc',
